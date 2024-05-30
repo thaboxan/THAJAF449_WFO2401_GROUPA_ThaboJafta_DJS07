b770f2abd3a6fc4531a4ce9618bbb398d6c151d7
Thabo <thvbojafta@gmail.com>
2024-05-30 18:13:11 +0200

    Meme.jsx html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header.jsx'
+import Meme from './components/Meme.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Meme />
   </React.StrictMode>,
 )
